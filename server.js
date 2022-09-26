d6c335572db3effbe9194cef9612c28c171da8a1
Justen Manni <mannijusten@gmail.com>
2022-09-26 23:40:43 +0800

    Update changes to server.js

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require("express");
+const { faker } = require("@faker-js/faker");
+const path = require("path");
+
 const app = express();
 const port = process.env.PORT ?? 3000;
 
 app.use(express.static("./client/dist/"));
 
-app.get("/api/", (req, res) => {
-  res.send("Hello World!");
+const todos = [1, 2, 3].map(() => {
+  return { title: faker.company.name() };
+});
+
+app.get("/api/todos", (req, res) => {
+  res.json(todos);
+});
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/dist", "index.html"));
 });
 
 app.listen(port, () => {
